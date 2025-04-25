1beed8d7fea03b1e1154fff68b00bbc8519cbf48
YusufBayam <yusufbayam@gmail.com>
2025-04-25 23:45:26 +0300

    deployment: add deployment script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+echo "Starting deployment..."
+
+# echo "Pulling latest code..."
+git pull origin main
+
+echo "Building and starting containers..."
+docker-compose -f docker-compose.prod.yaml up --build -d
+
+# echo "Running Django migrations..."
+# docker exec <your_api_container_name_or_id> python3 manage.py migrate
+
+# Optional: collect static files
+# docker exec <your_api_container_name_or_id> python3 manage.py collectstatic --noinput
+
+echo "Deployment finished!"
