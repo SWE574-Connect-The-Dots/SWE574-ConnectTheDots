3b3c9ce2ab0f01d335e4545e2a6c2058f7adbd5e
YusufBayam <yusufbayam@gmail.com>
2025-04-26 21:54:33 +0300

    deployment: add running migrations to deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@ set -e
 
 echo "Starting deployment..."
 
-# echo "Pulling latest code..."
+echo "Pulling latest code..."
 git pull origin main
 
 echo "Building and starting containers..."
 docker-compose -f docker-compose.prod.yaml up --build -d
 
-# echo "Running Django migrations..."
-# docker exec <your_api_container_name_or_id> python3 manage.py migrate
+echo "Running Django migrations..."
+docker exec swe573-api-1 python3 manage.py migrate
 
 # Optional: collect static files
 # docker exec <your_api_container_name_or_id> python3 manage.py collectstatic --noinput
