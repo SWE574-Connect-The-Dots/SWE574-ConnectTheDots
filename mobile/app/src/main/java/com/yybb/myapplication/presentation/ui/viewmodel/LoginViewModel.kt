9397e7d882198c3e478ff3954a283b125f7876e4
esranrzm <esranurozumm@gmail.com>
2025-10-11 18:48:49 +0300

    #25 add login screen added

--- mobile/app/src/main/java/com/yybb/myapplication/presentation/ui/viewmodel/LoginViewModel.kt
+++ mobile/app/src/main/java/com/yybb/myapplication/presentation/ui/viewmodel/LoginViewModel.kt
@@ -1,28 +1,56 @@
 package com.yybb.myapplication.presentation.ui.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.yybb.myapplication.R
+import com.yybb.myapplication.presentation.ui.utils.ViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() : ViewModel() {
+class LoginViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+) : ViewModel() {
 
     private val _eventChannel = Channel<AuthEvent>()
     val eventFlow = _eventChannel.receiveAsFlow()
+    private val _viewState = MutableStateFlow<ViewState<Unit>>(ViewState.Success(Unit))
+    val viewState: StateFlow<ViewState<Unit>> = _viewState
 
-    fun onLoginClicked() {
-        viewModelScope.launch {
-            _eventChannel.send(AuthEvent.NavigateToMain)
-        }
-    }
 
     fun onRegisterClicked() {
         viewModelScope.launch {
             _eventChannel.send(AuthEvent.NavigateToRegister)
         }
+    }
+
+    fun onLoginClicked(
+        username: String,
+        password: String
+    ) {
+        viewModelScope.launch {
+            _viewState.value = ViewState.Loading
+
+            when {
+                username.isBlank() || password.isBlank() ->
+                    _viewState.value = ViewState.Error(context.getString(R.string.fill_all_fileds_error))
+
+                else -> {
+                    _viewState.value = ViewState.Success(Unit)
+                    _eventChannel.send(AuthEvent.NavigateToMain)
+                }
+            }
+        }
+    }
+
+    fun clearError() {
+        _viewState.value = ViewState.Success(Unit)
     }
 }
