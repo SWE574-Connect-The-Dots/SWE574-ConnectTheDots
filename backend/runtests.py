9b0d6e331e9fd3f8678f010928a07da9f4bdcb88
Yasemin Tangül <ytangul1@ford.com.tr>
2025-10-16 10:48:17 +0300

    #1 change the importing in test wrapper

--- backend/runtests.py
+++ backend/runtests.py
@@ -1,16 +1,18 @@
 import os, sys, django
 from django.conf import settings
 from django.test.utils import get_runner
-import xmlrunner
+
+try:
+    from xmlrunner.extra.djangotestrunner import XMLTestRunner
+except ImportError:
+    # fallback for recent versions that moved it
+    from xmlrunner import XMLTestRunner
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
 TestRunner = get_runner(settings)
-test_runner = xmlrunner.extra.djangotestrunner.XMLTestRunner(
-    output='test-reports',  # directory for XML files
-    verbosity=2
-)
+test_runner = XMLTestRunner(output='test-reports', verbosity=2)
 
 failures = test_runner.run_tests(['api.tests'])
 sys.exit(bool(failures))
