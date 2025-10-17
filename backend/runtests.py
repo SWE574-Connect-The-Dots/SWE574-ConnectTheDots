bb60d4eb0e358251e583b8690a9de037e23bcfbd
Yasemin Tangül <ytangul1@ford.com.tr>
2025-10-17 16:07:35 +0300

    #1 update runner and create output directory for test reports

--- backend/runtests.py
+++ backend/runtests.py
@@ -1,23 +1,21 @@
-import os, sys, django
+import os
+import sys
+import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-try:
-    from xmlrunner.extra.djangotestrunner import XMLTestRunner
-except ImportError:
-    from xmlrunner import XMLTestRunner
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
+import xmlrunner
+
+
 TestRunner = get_runner(settings)
+runner = TestRunner(verbosity=2)
 
-# The key line: use Django's runner with xmlrunner integration
-test_runner = XMLTestRunner(
-    output='test-reports',  # this folder is relative to where this script runs
-    verbosity=2,
-)
+print("Running Django tests with XML output...")
+result = xmlrunner.XMLTestRunner(output='/app/test-reports', verbosity=2).run(runner.build_suite(['api.tests']))
 
-failures = test_runner.run_tests(['api.tests'])
-print("XMLTestRunner writing to:", os.path.abspath('test-reports'))
-sys.exit(bool(failures))
+print("XML reports written to /app/test-reports")
+sys.exit(not result.wasSuccessful())
