625b0511d63a139d520c5e6903a9e6a12f51678c
YusufBayam <yusufbayam@gmail.com>
2025-10-29 22:13:10 +0300

    #85 Fix migration overlap

--- backend/api/migrations/0012_edge_wikidata_property_id.py
+++ backend/api/migrations/0012_edge_wikidata_property_id.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0010_remove_null_statement_ids'),
+        ('api', '0011_profile_user_type_spacemoderator'),
     ]
 
     operations = [
